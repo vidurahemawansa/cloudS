54d411ec5e905287b963a73053fcb4e0bb13748e
Vidura Hemawansa <vishwanath.wgv2h@gmail.com>
2020-09-20 15:12:29 +0530

    added latest files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './components/menu/menu.component';
+import { ExpressionsComponent } from './components/expressions/expressions.component';
+import { FieldsComponent } from './components/fields/fields.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { ActionsComponent } from './components/actions/actions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent,
+    ExpressionsComponent,
+    FieldsComponent,
+    RulesComponent,
+    ActionsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/expressions/expressions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expressions',
+  templateUrl: './expressions.component.html',
+  styleUrls: ['./expressions.component.scss']
+})
+export class ExpressionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
